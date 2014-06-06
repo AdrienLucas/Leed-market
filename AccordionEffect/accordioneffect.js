5236e98d13c961aa3af651dfefe92c6696500def
Adrien Lucas <contact@adrienlucas.net>
2014-06-06 13:56:05 +0200

    Ajout du plugin AccordionEffect.

---
+++ AccordionEffect/accordioneffect.js
@@ -0,0 +1,32 @@
+(function($){
+	var articlesCount = 0,
+	accordionify = function (){
+		if($('article').length != articlesCount) {
+			articlesCount = $('article').each(function(){
+				if($(this).data('accordion-toggled') == null){
+					$(this)
+						.data('accordion-toggled', false)
+						.find('.articleContent').fadeOut(1).end()
+						.find('h1').prepend('<span class="accordion">[+]</span>');
+
+					$(this).find('.accordion')
+						.click(function(){
+							var button, $parent = $(this).closest('article');
+							if($parent.data('accordion-toggled')) {
+								$parent.find('.articleContent').fadeOut();
+								$parent.data('accordion-toggled', false);
+								button = '+';
+							} else {
+								$parent.find('.articleContent').fadeIn();
+								$parent.data('accordion-toggled', true);
+								button = '-';
+							}
+							$parent.find('.accordion').text('['+button+']');
+						});
+				}
+			}).length;
+		}
+	}
+	$(window).scroll(accordionify);
+	accordionify();
+})(jQuery);
---
+++ AccordionEffect/accordioneffect.plugin.disabled.php
@@ -0,0 +1,13 @@
+<?php
+/*
+@name Accordion Effect
+@author Adrien <contact@adrienlucas.net>
+@link http://adrienlucas.net/
+@licence WTFPL
+@version 1.0.0
+@description Used to collapse events contents
+@note jQuery is needed
+@note See attached screenshot.png
+*/
+
+Plugin::addJs("accordioneffect.js");
